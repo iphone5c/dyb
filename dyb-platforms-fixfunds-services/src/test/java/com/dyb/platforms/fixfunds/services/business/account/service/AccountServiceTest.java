8b9e5c6932516b92441899695bbe61b1983900d1
weiyuan <1612239320@qq.com>
2016-09-26 23:38:46 +0800

    菜单

--- dyb-platforms-fixfunds-services/src/test/java/com/dyb/platforms/fixfunds/services/business/account/service/AccountServiceTest.java
+++ dyb-platforms-fixfunds-services/src/test/java/com/dyb/platforms/fixfunds/services/business/account/service/AccountServiceTest.java
@@ -1,5 +1,8 @@
 package com.dyb.platforms.fixfunds.services.business.account.service;
 
+import com.dyb.platforms.fixfunds.services.business.account.entity.Account;
+import com.dyb.platforms.fixfunds.services.business.account.entity.em.AccountStatus;
+import com.dyb.platforms.fixfunds.services.business.account.entity.em.AccountType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,5 +18,15 @@ public class AccountServiceTest {
 
     @Test
     public void testGetAccountByCode() throws Exception {
+        Account account = new Account();
+        account.setPassword("123456");
+        account.setAccountName("admin");
+        account.setAccountPhone("666666");
+        account.setTradePassword("654321");
+        account.setAccountType(AccountType.信使);
+        account.setAccountForeignKey("1");
+        account.setReferrerCode("1");
+        account.setAccountStatus(AccountStatus.正常);
+        accountService.createAccount(account);
     }
 }

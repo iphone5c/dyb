a0ed85951b93466ea332d7ff7e3dedbd277de4c5
lixiaolong <913357884@qq.com>
2016-10-09 21:36:57 +0800

    获取菜单修改版

---
+++ dyb-webapp-fixfunds/web/client/js/home.js
@@ -0,0 +1,52 @@
+$(function(){
+    // 获取菜单
+    function getQueryString(name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) return decodeURI(r[2]); return null;
+    }
+    console.log(getQueryString("name"));
+    var param={
+        accountType:getQueryString("name")
+    };
+    var data = invokeService('/web/commons/getWebMenu',param);
+    var fhurl=new Array();
+    var addi=0;
+    console.log(data);
+    if (data.statusCode!=1000){
+        alert(data.errorMessage);
+        return;
+    }
+    for(var i=0;i<data.result.length;i++){
+        var html ='';
+        html+=
+            "<li class='js-menu-parent-li' style='position: relative'>" +
+            "<a class='js-menu-parent-li-a'>"+
+            "<i class='ad-icon sui-icon iconfonts  icon-tb-emoji' style='margin-top:14px;'></i>"+
+            "<span class='ad-menu-parent js-adShowHide'>"+data.result[i].text+"</span>"+
+            "<i class='iconfont icons1' style='font-size: 16px'>&#xe60b;</i>"+
+            "<i class='iconfont icons2'' style='font-size: 16px;display: none'>&#xe68b;</i>"+
+            "</a>"+
+            "<ul class='js-subMenu-absolute' style='display: none;'>";
+        for(var y=0;y<data.result[i].children.length;y++){
+            html+=
+                "<li>" +
+                "<a class='can_dispatch js-subActive'>"+data.result[i].children[y].text+"</a>" +
+                "</li>";
+            fhurl[addi]=data.result[i].children[y].url;
+            addi++;
+        }
+        html+="</ul></li>";
+        $("#leftmenu").html( $("#leftmenu").html()+html);
+    }
+    $("#leftmenu").on("click",".js-subActive",function(){
+        var ss= $(".js-subActive").index(this);
+        getpage1(fhurl[ss]);
+//        console.log(fhurl);
+    });
+    function getpage1(url){
+        $.get(url,function(data){
+            $("#ad-content").html(data);
+        })
+    }
+})

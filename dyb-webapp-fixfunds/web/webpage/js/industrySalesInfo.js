cb593d358253bd81aed059cb6b20e642e0ae81fe
tiantingsong <1359040110@qq.com>
2016-10-11 16:45:05 +0800

    订单列表页

--- dyb-webapp-fixfunds/web/webpage/js/industrySalesInfo.js
+++ dyb-webapp-fixfunds/web/webpage/js/industrySalesInfo.js
@@ -26,7 +26,7 @@ $(function(){
         $("#datePicker1").show();
         $(".zdydata").show();
     })
-    $(".btn-fontBlue1").click(function(){
+    $("#salesDetailtable").on("click",".btn-fontBlue1",function(){
         $(".sui-modal").addClass("in");
         $(".sui-modal-backdrop").css("zIndex","1").addClass("in");
     });
@@ -35,7 +35,7 @@ $(function(){
         $(".sui-modal-backdrop").css("zIndex","-1").removeClass("in");
     })
 //订单管理分页列表
-    var a=0
+    var a=0;
     var param={
         // 当前页
         pageIndex:a,
@@ -47,5 +47,169 @@ $(function(){
         alert(result.errorMessage);
         return;
     }
-    console.log(result)
-})
+    console.log(result);
+
+    $("#xg_page").text("共 " +result.result.pageCount+ " 页");
+    $("#onpage").text("第 " +(a+1)+ " 页");
+    for(var i =0;i<result.result.list.length;i++){
+        $("#salesDetailtable>table>tbody").html($("#salesDetailtable>table>tbody").html()+
+                "<tr>"+
+                "<td>"+
+                    result.result.list[i].order.tradeTime+
+                "</td>"+
+                "<td>" +
+                result.result.list[i].order.memberCode+
+                "</td>"+
+                "<td>"+
+                result.result.list[i].account.member.realName+
+                "</td>"+
+                "<td>"+
+                result.result.list[i].account.accountPhone+
+                "</td>"+
+                "<td>"+
+                result.result.list[i].order.price+
+                "</td>"+
+                "<td>"+
+                result.result.list[i].order.incentiveMode+
+                "</td>"+
+                "<td>"+
+                "<a class='btn_xql'>"+
+                result.result.list[i].order.orderCode+
+                "</a>"+
+                "</td>"+
+                "<td>"+
+                result.result.list[i].order.status+
+                "</td>"+
+                "<td>"+
+                ""+
+                "</td>"+
+                "</tr>"
+        )
+    }
+//    订单详情列表
+    $("#salesDetailtable").on("click",".btn_xql",(function(){
+        var i = $(".btn_xql").index(this);
+        alert($(".btn_xql").eq(i).text());
+    }))
+
+
+
+
+
+
+     //      下一页
+    $("#nextBtn").click(function(){
+        a++;
+        var param={
+            // 当前页
+            pageIndex:a,
+            //每页显示条数
+            pageSize:5
+        };
+        var result = invokeService('/web/merchant/commodity/getOrderPageList',param);
+        if(result.statusCode!=1000){
+            alert(result.errorMessage)
+            return;
+        }
+        if(a<result.result.pageCount){
+            $("#onpage").text("第 " +(a+1)+ " 页");
+            $("#salesDetailtable>table>tbody").html("");
+            for(var i =0;i<result.result.list.length;i++){
+                $("#salesDetailtable>table>tbody").html($("#salesDetailtable>table>tbody").html()+
+                        "<tr>"+
+                        "<td>"+
+                        result.result.list[i].order.tradeTime+
+                        "</td>"+
+                        "<td>" +
+                        result.result.list[i].order.memberCode+
+                        "</td>"+
+                        "<td>"+
+                        result.result.list[i].account.member.realName+
+                        "</td>"+
+                        "<td>"+
+                        result.result.list[i].account.accountPhone+
+                        "</td>"+
+                        "<td>"+
+                        result.result.list[i].order.price+
+                        "</td>"+
+                        "<td>"+
+                        result.result.list[i].order.incentiveMode+
+                        "</td>"+
+                        "<td>"+
+                        result.result.list[i].order.orderCode+
+                        "</td>"+
+                        "<td>"+
+                        result.result.list[i].order.status+
+                        "</td>"+
+                        "<td>"+
+                        ""+
+                        "</td>"+
+                        "</tr>"
+                )
+            }
+//											window.location.href="http://localhost:63342/untitled/comm/client/index.html"
+        }
+        else{
+            a=result.result.pageCount;
+            a--;
+        }
+    });
+				//		上一页
+    $("#prevBtn").click(function(){
+        a--;
+        if(a<0){
+            a=0;
+        }
+        var param={
+            // 当前页
+            pageIndex:a,
+            //每页显示条数
+            pageSize:5
+        };
+        var result = invokeService('/web/merchant/commodity/getOrderPageList',param);
+        if(result.statusCode!=1000){
+            alert(result.errorMessage)
+            return;
+        }
+        if(a>=0){
+            $("#onpage").text("第 " +(a+1)+ " 页");
+            $("#salesDetailtable>table>tbody").html("");
+            for(var i =0;i<result.result.list.length;i++){
+                $("#salesDetailtable>table>tbody").html($("#salesDetailtable>table>tbody").html()+
+                        "<tr>"+
+                        "<td>"+
+                        result.result.list[i].order.tradeTime+
+                        "</td>"+
+                        "<td>" +
+                        result.result.list[i].order.memberCode+
+                        "</td>"+
+                        "<td>"+
+                        result.result.list[i].account.member.realName+
+                        "</td>"+
+                        "<td>"+
+                        result.result.list[i].account.accountPhone+
+                        "</td>"+
+                        "<td>"+
+                        result.result.list[i].order.price+
+                        "</td>"+
+                        "<td>"+
+                        result.result.list[i].order.incentiveMode+
+                        "</td>"+
+                        "<td>"+
+                        result.result.list[i].order.orderCode+
+                        "</td>"+
+                        "<td>"+
+                        result.result.list[i].order.status+
+                        "</td>"+
+                        "<td>"+
+                        ""+
+                        "</td>"+
+                        "</tr>"
+                )
+            }
+        }
+        else{
+
+        }
+    });
+});

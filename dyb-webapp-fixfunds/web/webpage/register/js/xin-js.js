180b0942c54f7579764399d1ee1c84cb2bf02b53
weiyuan <1612239320@qq.com>
2016-10-17 14:33:31 +0800

    信使注册修改

--- dyb-webapp-fixfunds/web/webpage/register/js/xin-js.js
+++ dyb-webapp-fixfunds/web/webpage/register/js/xin-js.js
@@ -24,10 +24,9 @@ $(function(){
     if (data.statusCode==1000){
         $("#referrerName").html(data.result.name)
         $("#referrerMobile").html(data.result.phone)
-        return;
     }
 
-$(".xin_submit").click(function(){
+$("#nextReg2").click(function(){
         // 账户名
         var accountName=$("#username").val();
         // 账户密码
@@ -86,22 +85,9 @@ $(".xin_submit").click(function(){
 }
     console.log(result)
     console.log(param)
-
-
-
-//    $.ajax({
-//        url:"http://192.168.0.113:8080/web/member/registerMemberAccount",
-//        type:"post",
-//        dataType: 'JSONP',
-//        jsonp: "callbackparam", //服务端用于接收callback调用的function名的参数
-//        jsonpCallback: "web", //callback的function名称,服务端会把名称和data一起传递回来
-//        data: {
-//
-//        },
-//        success: function (data) {
-//            console.log(data)
-//        }
-//    })
+    $(".ad-register1").css({"display":"none"});
+    $(".ad-register2").css({"display":"none"});
+    $(".ad-register3").css({"display":"block"})
 })
 
 
